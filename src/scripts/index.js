ac6e222aba23736d6564cf7199ea1b2d88529926
scrooge <etoerik@gmail.com>
2020-10-19 23:50:09 +0300

    Add FormValidator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,30 @@
 import "../pages/index.css";
+import Popup from './components/Popup';
+import FormValidator from "./components/FormValidator";
+
+const popupSignup = document.querySelector('.popup-signup');
+const formSignup = document.forms.formSignup;
+const newEmail = formSignup.elements.newEmail;
+const newPassword = formSignup.elements.newPassword;
+const newName = formSignup.elements.newName;
+const buttonAuthorization = document.querySelector('.menu__authorization-button');
+const buttonPopupSignupClose = document.querySelector('.signup-popup__close');
+
+function handlerRenderPopupSignup() {
+  const popup = new Popup(popupSignup);
+  const popupButton = formSignup.querySelector('.popup__button');
+  const formValidator = new FormValidator(formSignup);
+
+  formValidator.setEventListeners()
+  // popupButton.setAttribute('disabled', "");
+  formValidator.resetError(formSignup);
+  formSignup.reset();
+  popup.open();
+}
+
+buttonPopupSignupClose.addEventListener('click', () => {
+  const popup = new Popup(popupSignup);
+  popup.close();
+});
+
+buttonAuthorization.addEventListener('click', handlerRenderPopupSignup);
