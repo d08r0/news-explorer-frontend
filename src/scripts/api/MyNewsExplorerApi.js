b56de65d475d02a5fbbca594c0fc44bfb42e96d4
scrooge <etoerik@gmail.com>
2020-10-21 01:20:15 +0300

    Add signin

--- src/scripts/api/MyNewsExplorerApi.js
+++ src/scripts/api/MyNewsExplorerApi.js
@@ -1,12 +1,10 @@
 export default class MyNewsExplorerApi {
-  // constructor(baseUrl) {
-  //   this.url = baseUrl;
-  // }
 
   constructor(config) {
     this.url = config.url;
   }
 
+  // Регистрация
   signup(userEmail, userPassword, userName) {
 
     return fetch(`${this.url}/signup`, {
@@ -25,6 +23,34 @@ export default class MyNewsExplorerApi {
           return res.json();
         }
         return Promise.reject(res.status);
+      });
+  }
+
+  // Вход
+  signin(userEmail, userPassword) {
+    return fetch(`${this.url}/signin`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: userEmail,
+        password: userPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res.status);
+      })
+      .then((data) => {
+        localStorage.setItem('token', data.userToken);
+        localStorage.setItem('user', JSON.stringify({
+          email: data.email,
+          name: data.name,
+        }));
+        return data;
       });
   }
 }
